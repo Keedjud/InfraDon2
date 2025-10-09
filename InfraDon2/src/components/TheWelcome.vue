8fc86337e5e530fb1ae72e0c9839b5deef3cb100
Keedjud <inoewenger@gmail.com>
2025-10-09 16:23:22 +0200

    Cours2

--- InfraDon2/src/components/TheWelcome.vue
+++ InfraDon2/src/components/TheWelcome.vue
@@ -1,5 +1,24 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import PouchDB from 'pouchdb'
+import { onMounted, ref } from 'vue';
+
+const storage = ref();
+
+onMounted(() => {
+  console.log('=> Composant initialisé');
+  initDatabase()
+});
+
+const initDatabase = () => {
+  console.log('=> Connexion à la base de données');
+  const db = new PouchDB('http://inoe.wenger:IWtramp54HEIG/@localhost:5984/')
+  if (db) {
+    console.log("Connected to collection : " + db?.name)
+    storage.value = db
+  } else {
+    console.warn('Something went wrong')
+  }
+}
 
 // DATA - MODEL
 const counter = ref(20)
