94f2ddcb4bf67957bc0b43fefdf2be9599ca33e2
Keedjud <inoewenger@gmail.com>
2025-10-30 14:01:17 +0100

    Ajout FetchData et CreateDoc dans TheWelcome.vue

--- InfraDon2/src/components/TheWelcome.vue
+++ InfraDon2/src/components/TheWelcome.vue
@@ -2,16 +2,26 @@
 import PouchDB from 'pouchdb'
 import { onMounted, ref } from 'vue';
 
+declare interface Post {
+  title: string
+  content: string
+  attributes: {
+    creation_date: any
+  }
+}
+
 const storage = ref();
+const postsData = ref<Post[]>([])
 
 onMounted(() => {
   console.log('=> Composant initialisé');
   initDatabase()
+  fetchData()
 });
 
 const initDatabase = () => {
   console.log('=> Connexion à la base de données');
-  const db = new PouchDB('http://inoe.wenger:IWtramp54HEIG/@localhost:5984/')
+  const db = new PouchDB('http://inoe.wenger:IWtramp54HEIG/@localhost:5984/infradon_inoe_db/')
   if (db) {
     console.log("Connected to collection : " + db?.name)
     storage.value = db
@@ -20,18 +30,41 @@ const initDatabase = () => {
   }
 }
 
-// DATA - MODEL
-const counter = ref(20)
-
-// METHODS - CONTROLLER
-const increment = () => {
-  counter.value++
+const fetchData = (): any => {
+  storage.value
+    .allDocs({
+      include_docs: true,
+    })
+    .then((result: any) => {
+      console.log('=> Données récupérées :', result.rows)
+      postsData.value = result.rows.map((row: any) => row.doc)
+    })
+    .catch((error: any) => {
+      console.error('Erreur lors de la récupération des données :', error)
+    })
 }
 
+const createDoc = (): any => {
+  storage.value
+    .post({
+      title: 'Document',
+      content: 'Contenu du document ',
+    })
+    .then(function (response: any) {
+      fetchData()
+      console.log(response)
+    })
+    .catch(function (err: any) {
+      console.log(err)
+    })
+}
 </script>
 
 <template>
-  <h1>Hello World</h1>
-  <button @click="increment">Increment</button>
-  <p style="color: white; font-size: 50px;">{{ counter }}</p>
+  <h1>Fetch Data</h1>
+  <article v-for="post in postsData" v-bind:key="(post as any).id">
+    <h2>{{ post.title }}</h2>
+    <p>{{ post.content }}</p>
+  </article>
+  <button @click="createDoc">Ajouter un document</button>
 </template>
